f7a93bec679a3fdfc641c55aa16b1dad95e2fabc
Zac Messsinger <zmessinger@quantummetric.com>
2019-10-08 20:34:05 -0700

    Finished Plinko example

--- plinko/score.js
+++ plinko/score.js
@@ -6,22 +6,31 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 
 function runAnalysis() {
 	const testSetSize = 100;
-	const [testSet, trainingSet] = splitDataSet(outputs, testSetSize);
+	const k = 10;
 	
-	_.range(1,20).forEach(k => {
+	_.range(0,3).forEach(feature => {
+		// feature === 0, feature === 1, feature === 2
+		const data = _.map(outputs, row => [row[feature], _.last(row)]);
+		const [testSet, trainingSet] = splitDataSet(minMax(data, 1), testSetSize);
 		const accuracy = _.chain(testSet)
-			.filter(testPoint => knn(trainingSet, testPoint[0], k) === testPoint[3])
+			.filter(testPoint => knn(trainingSet, _.initial(testPoint), k) === _.last(testPoint))
 			.size()
 			.divide(testSetSize)
 			.value();
 
-		console.log(`For k of ${k}`,`Accuracy is ${accuracy}`);
+		console.log(`For feature of ${feature}`,`Accuracy is ${accuracy}`);
 	})
 };
 
 function knn(data, point, k) {
+	// point has 3 values!!
 	return _.chain(data)
-		.map(row => [distance(row[0], point), row[3]])
+		.map(row => {
+			return [
+				distance(_.initial(row), point),
+				_.last(row)
+			];
+		})
 		.sortBy(row => row[0])
 		.slice(0, k)
 		.countBy(row => row[1])
@@ -34,7 +43,11 @@ function knn(data, point, k) {
 }
 
 function distance(pointA, pointB) {
-	return Math.abs(pointA - pointB);
+	return _.chain(pointA) // Start Sequence
+		.zip(pointB) // Map Arrays into New Arrays => [1,2] [3,4] => [1,3] [2,4]
+		.map(([a,b]) => (a - b) ** 2) // 
+		.sum()
+		.value() ** 0.5;
 };
 
 // Split Data set into test set and data set.
@@ -45,4 +58,21 @@ function splitDataSet(data, testCount) {
 	const trainingSet = _.slice(shuffled, testCount);
 
 	return [testSet, trainingSet];
+};
+
+function minMax(data, featureCount) {
+	const clonedData = _.cloneDeep(data);
+
+	for (let i=0; i<featureCount; i++) {
+		const column = clonedData.map(row => row[i]);
+
+		const min = _.min(column);
+		const max = _.max(column);
+
+		for (let j=0; j < clonedData.length; j++) {
+			clonedData[j][i] = (clonedData[j][i] - min) / (max -min);
+		}
+	};
+
+	return clonedData;
 };
